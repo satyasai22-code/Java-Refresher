e863b92fb4d459780d104d9cc885e9f943925432
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-03-22 18:43:12 +0530

    KthSmallestAndLargest is added

---
+++ programs/week-1/KthSmallestAndLargest.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+/**
+ * KthSmallestAndLargest
+ */
+public class KthSmallestAndLargest {
+
+    public static Integer[] solve(Integer[] arr, int k){
+        Arrays.sort(arr);
+        return new Integer[]{arr[k - 1], arr[arr.length - k ]};
+    }
+    public static void main(String[] args) {
+        Integer[] arr = {1, 4, 5, 7, 9};
+        int k = 2;
+        for(Integer i: solve(arr, k)){
+            System.out.println(i);
+        }
+    }
+}
