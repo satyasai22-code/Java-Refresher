0ac3071a7d8306d879794b97ec1f9f70bb30ff71
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-03-23 16:29:40 +0530

    updated inheritance

--- programs/week-1/AccessModifiers.java
+++ programs/week-1/AccessModifiers.java
@@ -1,6 +1,9 @@
 class Data {
     private String name;
 
+    protected String getClassName(){
+        return "Data";
+    }
     public String getName() {
         return this.name;
     }
@@ -8,10 +11,18 @@ class Data {
         this.name= name;
     }
 }
+class SubData extends Data{
+    @Override
+    public String getClassName(){
+        return super.getClassName();
+    }
+}
 public class AccessModifiers {
     public static void main(String[] main){
         Data d = new Data();
         d.setName("Satya");
         System.out.println(d.getName());
+        SubData subData = new SubData();
+        System.out.println(subData.getClassName());
     }
 }
