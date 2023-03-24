e17f5b7ad6381c99fe83175dcacb8c624eebebfc
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-03-24 16:06:35 +0530

    Add DynamicObject creation class

---
+++ programs/week-1/DynamicClassLoader.java
@@ -0,0 +1,21 @@
+import javax.management.ObjectName;
+
+class Animal{
+    public void run(){
+        System.out.println("Inside run method of Animal Class " );
+    }
+}
+public class DynamicClassLoader extends ClassLoader{
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+            String className = "Animal";
+            ClassLoader classLoader = DynamicClassLoader.class.getClassLoader();
+            Class loadedClass = classLoader.loadClass(className);
+            Object obj = loadedClass.getConstructor().newInstance();
+            ((Animal)obj).run();
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
+    
+    }
+}
