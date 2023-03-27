a31da98f99be52754854c6d0f12d7693f41858db
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-03-27 11:03:41 +0530

    Update String Class

--- programs/src/main/java/com/javarefresher/week01/StringClass.java
+++ programs/src/main/java/com/javarefresher/week01/StringClass.java
@@ -4,13 +4,15 @@ public class StringClass {
     public static void main(String[] args) {
         String s1 = new String("HEllo");//2 objects will be created.1 in heap and 1 in SP
         s1.concat("Satya");//1 object will be created in SP as "Satya" is new literal and s1 will be "Hello" as strings are immutable
-        s1 = s1.concat("Satya");//Only 1 Object will be created in Heap as "Satya" is present in SP and s1 will be "HelloSatya"
-        
-        String s2 = new String("lowercase");
-        //String Methods
         System.out.println(s1);
+        s1 = s1.concat("Satya");//Only 1 Object will be created in Heap as "Satya" is present in SP and s1 will be "HelloSatya"
+        System.out.println(s1);
+
+        //String Methods
         System.out.println("Char at first index: " + s1.charAt(1));
         System.out.println("is s1 empty: " + s1.isEmpty());
+
+        String s2 = new String("lowercase");
         System.out.println("s1 == s1.toLowerCase(): " +( s1 == s1.toLowerCase())); //If there is a change required upon method calling, then new object is created
         System.out.println("s2 == s2.toLowerCase(): " +(s2 == s2.toLowerCase()));// If there is no change required upon method calling, then no new object will be created
 
