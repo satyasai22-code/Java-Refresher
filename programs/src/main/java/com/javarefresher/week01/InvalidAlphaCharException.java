216ed7aee2b64601d1f2188957a444d7e08bfb15
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-03-27 10:26:33 +0530

    Added excepion statements

---
+++ programs/src/main/java/com/javarefresher/week01/InvalidAlphaCharException.java
@@ -0,0 +1,9 @@
+package com.javarefresher.week01;
+
+public class InvalidAlphaCharException extends Exception {
+
+    public InvalidAlphaCharException(String message) {
+        super(message);
+    }
+    
+}
--- programs/src/main/java/com/javarefresher/week01/MaxFrequemcyChar.java
+++ programs/src/main/java/com/javarefresher/week01/MaxFrequemcyChar.java
@@ -4,27 +4,47 @@ public class MaxFrequemcyChar {
         return ch - 97;
     }
     static int CHAR_COUNT_SIZE = 26;
-    static char getMaxOccurringChar(String s)
+    // Only small case enlgish letters are allowed
+    static char getMaxOccurringChar(String s) throws InvalidAlphaCharException
     {
-        int count[] = new int[CHAR_COUNT_SIZE];
-        int len = s.length();
-        for (int i = 0; i < len; i++)
-            count[getIndexOfChar(s.charAt(i))]++;
- 
-        int max = 0;
-        char result = ' '; 
-        for (int i = 0; i < len; i++) {
-            if (max < count[getIndexOfChar(s.charAt(i))]) {
-                max = count[getIndexOfChar(s.charAt(i))];
-                result = s.charAt(i);
+        try {
+            int count[] = new int[CHAR_COUNT_SIZE];
+            int len = s.length();
+            for (int i = 0; i < len; i++){
+                char ch = s.charAt(i);
+                if(ch>=65 && ch<=90){
+                    throw new InvalidAlphaCharException(ch + " is not an small case letter");
+                }
+                if(ch<97 || ch>122){
+                    throw new InvalidAlphaCharException(ch + " is not an english alphabet");
+                }
+    
+                count[getIndexOfChar(ch)]++;
             }
+     
+            int max = 0;
+            char result = ' '; 
+            for (int i = 0; i < len; i++) {
+                if (max < count[getIndexOfChar(s.charAt(i))]) {
+                    max = count[getIndexOfChar(s.charAt(i))];
+                    result = s.charAt(i);
+                }
+            }
+            return result;
+        }catch(InvalidAlphaCharException e){
+            System.out.println(e.getMessage());
+        } 
+        catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        return result;
+        return 0;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidAlphaCharException {
         System.out.println(getMaxOccurringChar("null"));
         System.out.println(getMaxOccurringChar("abcde"));
         System.out.println(getMaxOccurringChar("wwwxxxyyyzzz"));
         System.out.println(getMaxOccurringChar("wwwwxxxyyyzzz"));
+        System.out.println(getMaxOccurringChar("Wwwwxxxyyyzzz"));
+        System.out.println(getMaxOccurringChar("w@wwwxxxyyyzzz"));
     }
 }
