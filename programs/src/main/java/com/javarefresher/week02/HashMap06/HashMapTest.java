4fcc7ec3d840d3428374d61ef6fa4f44660697b5
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-04-04 20:50:37 +0530

    Update with Object

--- programs/src/main/java/com/javarefresher/week02/HashMap06/HashMapTest.java
+++ programs/src/main/java/com/javarefresher/week02/HashMap06/HashMapTest.java
@@ -6,43 +6,46 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import com.javarefresher.week02.Person;
 
 public class HashMapTest {
-    public static Map<String, Integer> sortHashMapByValuesByTreeMap(Map<String, Integer> map){
-        TreeMap<String, Integer> treeMap = new TreeMap<>(map);
-        Map<String, Integer> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> pair : treeMap.entrySet()){
+    public static Map<String, Person> sortHashMapByValuesByTreeMap(Map<String, Person> map){
+        TreeMap<String, Person> treeMap = new TreeMap<>(map);
+        Map<String, Person> sortedMap = new LinkedHashMap<>();
+        for (Entry<String, Person> pair : treeMap.entrySet()){
             sortedMap.put(pair.getKey(), pair.getValue());
             // System.out.print(pair.getKey() + " : " + pair.getValue() + " ");
         }
         return sortedMap;
     }
-    public static Map<String, Integer> sortUsingArray(Map<String, Integer> map)
+    public static Map<String, Person> sortUsingArray(Map<String, Person> map)
     {
         ArrayList<String> sortedKeys
             = new ArrayList<String>(map.keySet());
  
         Collections.sort(sortedKeys);
-        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        Map<String, Person> sortedMap = new LinkedHashMap<>();
         for (String x : sortedKeys)
              sortedMap.put(x, map.get(x));
         return sortedMap;
     }
     public static void main(String[] args) {
-        Map<String, Integer> hashMap = new HashMap<>();
-        hashMap.put("dab", 100);
-        hashMap.put("dac", 98);
-        hashMap.put("dacb", 99);
-        hashMap.put("dacba", 99);
-        hashMap.put("a", 97);
+        Map<String, Person> hashMap = new HashMap<>();
+        hashMap.put("dab",   new Person("Jay", 24));
+        hashMap.put("dac",  new Person("Sai", 21));
+        hashMap.put("dacb", new Person("Satya", 20));
+        hashMap.put("dacba", new Person("Jai", 23));
+        hashMap.put("a",  new Person("Neelapala", 20));
         System.out.println("Before sorting: " + hashMap);
         System.out.println("After sorting: " +sortHashMapByValuesByTreeMap(hashMap));
-        Map<String, Integer> map = new HashMap<>();
-        map.put("Jay", 80);
-        map.put("Abhiram", 90);
-        map.put("Anvesh", 80);
-        map.put("Amit", 75);
-        map.put("Satya", 40);
+        Map<String, Person> map = new HashMap<>();
+        map.put("Jay",  new Person("Jay", 24));
+        map.put("Abhiram",  new Person("Abhiram", 21));
+        map.put("Anvesh", new Person("Anvesh", 20));
+        map.put("Amit", new Person("Amit", 23));
+        map.put("Satya", new Person("Neelapala", 20));
         System.out.println("Before sorting: " + map);
         System.out.println("After sorting using Treemap: " + sortHashMapByValuesByTreeMap(map));
         System.out.println("After sorting using ArrayList: " + sortUsingArray(map));
