bf435161c09da02b0d63deec43290b92e199e599
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-04-12 18:52:26 +0530

    Add executors

---
+++ programs/src/main/java/com/javarefresher/week03/Executors05/FixedThreadPool.java
@@ -0,0 +1,53 @@
+package com.javarefresher.week03.Executors05;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class Task implements Callable<String> {
+    private String message;
+ 
+    public Task(String message)
+    {
+        this.message = message;
+    }
+ 
+    public String call() throws Exception
+    {
+        return "Hi " + message + "!";
+    }
+}
+
+public class FixedThreadPool {
+
+    public static void main(String[] args)
+    {
+
+        Task task = new Task("Task1");
+ 
+        // Creating object of ExecutorService class and
+        // Future object Class
+        ExecutorService executorService
+            = Executors.newFixedThreadPool(4);
+        Future<String> result
+            = executorService.submit(task);
+ 
+        try {
+            System.out.println(result.get());
+        }
+ 
+        catch (InterruptedException
+               | ExecutionException e) {
+ 
+            // Display message only
+            System.out.println(
+                "Error occurred while executing the submitted task");
+ 
+            e.printStackTrace();
+        }
+ 
+        executorService.shutdown();
+    }
+}
