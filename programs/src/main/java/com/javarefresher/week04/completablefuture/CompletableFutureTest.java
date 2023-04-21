ccb42796c7bab69db4a313493ff836bf43716216
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-04-21 18:35:01 +0530

    Add completable code

---
+++ programs/src/main/java/com/javarefresher/week04/completablefuture/CompletableFutureTest.java
@@ -0,0 +1,21 @@
+package com.javarefresher.week04.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureTest {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of the asynchronous computation";
+        });
+        
+        String result = future.get();
+        System.out.println(result);
+    }
+}
