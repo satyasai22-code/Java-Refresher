b3fc3644551da31bc98f2e0dded197a8e6d82414
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-04-28 11:02:37 +0530

    Add JVM architecture and design patterns

---
+++ programs/src/main/java/com/javarefresher/week05/designpatterns/creationaldesignpatterns/factorypattern/FactoryPattern.java
@@ -0,0 +1,27 @@
+package com.javarefresher.week05.designpatterns.creationaldesignpatterns.factorypattern;
+
+/*
+ Factory Method - It is a creational design pattern that provides
+ an interface for creating objects in a superclass, but allows
+ subclasses to alter the type of objects that will be created
+*/
+
+/*
+Why (Problem its addressing)?
+Adding a new class to the program isn’t that simple if the rest of the code
+is already coupled to existing classes. 
+*/
+
+/*
+ * How (Solution it provides)?
+ * The Factory Method pattern suggests that you replace direct
+ * object construction calls (using the new operator) with calls
+ * to a special factory method. Don’t worry: the objects are still
+ * created via the new operator, but it’s being called from within
+ * the factory method. Objects returned by a factory method are
+ * often referred to as “products.
+ */
+
+public class FactoryPattern {
+    
+}
