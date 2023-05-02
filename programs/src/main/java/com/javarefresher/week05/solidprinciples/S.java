8d99a675416355f2357b491479a5eb10039fb43b
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-05-02 15:57:50 +0530

    Add S principle

--- programs/src/main/java/com/javarefresher/week05/solidprinciples/S.java
+++ programs/src/main/java/com/javarefresher/week05/solidprinciples/S.java
@@ -1,5 +1,54 @@
 package com.javarefresher.week05.solidprinciples;
 
+
+// class Book {
+
+//     private String name;
+//     private String author;
+//     private String text;
+
+//     //constructor, getters and setters
+
+//     // methods that directly relate to the book properties
+//     public String replaceWordInText(String word, String replacementWord){
+//         return text.replaceAll(word, replacementWord);
+//     }
+
+//     // methods for outputting text
+//     void printTextToConsole(String text){
+//         return ;
+//     }
+
+//     void printTextToAnotherMedium(String text){
+//         return;
+//     }
+// }
+// Above code not only stores book info but also prints in 2 ways.
+// So above class managing 2 responsibilities which is inefficient
+
+//Solution:
+class Book {
+
+    private String name;
+    private String author;
+    private String text;
+
+    // methods that directly relate to the book properties
+    public String replaceWordInText(String word, String replacementWord){
+        return text.replaceAll(word, replacementWord);
+    }
+}
+
+class BookPrinter{
+    // methods for outputting text
+    void printTextToConsole(String text){
+        return ;
+    }
+    void printTextToAnotherMedium(String text){
+        return;
+    }
+}
+
 //A class should have one and only one reason to change, meaning that a class should have only one job.
 public class S {
     
