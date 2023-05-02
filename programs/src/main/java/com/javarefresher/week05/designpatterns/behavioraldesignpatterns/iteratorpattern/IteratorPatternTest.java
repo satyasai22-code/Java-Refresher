d0a5e889527d2de2330841ba1b952fbce9c6488f
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2023-05-02 15:24:48 +0530

    add iterator pattern

---
+++ programs/src/main/java/com/javarefresher/week05/designpatterns/behavioraldesignpatterns/iteratorpattern/IteratorPatternTest.java
@@ -0,0 +1,135 @@
+package com.javarefresher.week05.designpatterns.behavioraldesignpatterns.iteratorpattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+enum ChannelTypeEnum {
+	ENGLISH, HINDI, FRENCH, ALL;
+}
+
+class Channel {
+
+	private double frequency;
+	private ChannelTypeEnum TYPE;
+	
+	public Channel(double freq, ChannelTypeEnum type){
+		this.frequency=freq;
+		this.TYPE=type;
+	}
+
+	public double getFrequency() {
+		return frequency;
+	}
+
+	public ChannelTypeEnum getTYPE() {
+		return TYPE;
+	}
+	
+	@Override
+	public String toString(){
+		return "Frequency="+this.frequency+", Type="+this.TYPE;
+	}
+}
+
+interface ChannelCollection {
+
+	public void addChannel(Channel c);
+	
+	public void removeChannel(Channel c);
+	
+	public ChannelIterator iterator(ChannelTypeEnum type);
+	
+}
+
+interface ChannelIterator {
+
+	public boolean hasNext();
+	
+	public Channel next();
+}
+
+class ChannelCollectionImpl implements ChannelCollection {
+
+	private List<Channel> channelsList;
+
+	public ChannelCollectionImpl() {
+		channelsList = new ArrayList<>();
+	}
+
+	public void addChannel(Channel c) {
+		this.channelsList.add(c);
+	}
+
+	public void removeChannel(Channel c) {
+		this.channelsList.remove(c);
+	}
+
+	@Override
+	public ChannelIterator iterator(ChannelTypeEnum type) {
+		return new ChannelIteratorImpl(type, this.channelsList);
+	}
+
+	private class ChannelIteratorImpl implements ChannelIterator {
+
+		private ChannelTypeEnum type;
+		private List<Channel> channels;
+		private int position;
+
+		public ChannelIteratorImpl(ChannelTypeEnum ty,
+				List<Channel> channelsList) {
+			this.type = ty;
+			this.channels = channelsList;
+		}
+
+		@Override
+		public boolean hasNext() {
+			while (position < channels.size()) {
+				Channel c = channels.get(position);
+				if (c.getTYPE().equals(type) || type.equals(ChannelTypeEnum.ALL)) {
+					return true;
+				} else
+					position++;
+			}
+			return false;
+		}
+
+		@Override
+		public Channel next() {
+			Channel c = channels.get(position);
+			position++;
+			return c;
+		}
+
+	}
+}
+
+public class IteratorPatternTest {
+    public static void main(String[] args) {
+        ChannelCollection channels = populateChannels();
+		ChannelIterator baseIterator = channels.iterator(ChannelTypeEnum.ALL);
+		while (baseIterator.hasNext()) {
+			Channel c = baseIterator.next();
+			System.out.println(c.toString());
+		}
+		// Channel Type Iterator
+		ChannelIterator englishIterator = channels.iterator(ChannelTypeEnum.ENGLISH);
+		while (englishIterator.hasNext()) {
+			Channel c = englishIterator.next();
+			System.out.println(c.toString());
+		}
+    }
+
+    private static ChannelCollection populateChannels() {
+		ChannelCollection channels = new ChannelCollectionImpl();
+		channels.addChannel(new Channel(98.5, ChannelTypeEnum.ENGLISH));
+		channels.addChannel(new Channel(99.5, ChannelTypeEnum.HINDI));
+		channels.addChannel(new Channel(100.5, ChannelTypeEnum.FRENCH));
+		channels.addChannel(new Channel(101.5, ChannelTypeEnum.ENGLISH));
+		channels.addChannel(new Channel(102.5, ChannelTypeEnum.HINDI));
+		channels.addChannel(new Channel(103.5, ChannelTypeEnum.FRENCH));
+		channels.addChannel(new Channel(104.5, ChannelTypeEnum.ENGLISH));
+		channels.addChannel(new Channel(105.5, ChannelTypeEnum.HINDI));
+		channels.addChannel(new Channel(106.5, ChannelTypeEnum.FRENCH));
+		return channels;
+	}
+}
